8f6470b38ebd9cd2fbe6c5584074a9cb2ef03cc7
sumukh-vasisht <sumukhmys1999@gmail.com>
2020-05-21 12:31:58 +0530

    Upload

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, render_template, redirect, url_for 
 # import MySQLdb
 import os
+from pyzbar import pyzbar
+
+uploadFolder = 'qrCodes'
 
 app = Flask(__name__)
+
+app.config['uploadFolder'] = uploadFolder
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -27,6 +32,15 @@ def register():
 
 @app.route('/guestLogin', methods=['GET', 'POST'])
 def guestLogin():
+    if request.method=="POST":
+        qrcode = request.files['qrcode']
+        if qrcode.filename=='':
+            message = "No image captured"
+            return redirect('guestLogin', confirm=message)
+        else:
+            qrcode.save(os.path.join(app.config['uploadFolder'], filename))
+            message = "File uploaded successfully"
+            return redirect('guestLogin', confirm=message)
     return render_template("guestLogin.html")
 
 if __name__ == "__main__":
