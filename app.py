94ab5854a4a66b5a12c887ba7fc4e7c4b27d4d1d
sumukh-vasisht <sumukhmys1999@gmail.com>
2020-05-21 11:23:33 +0530

    Guest login

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for 
+import MySQLdb
 import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    # if request.method=="POST":
-    #     global team1, team2
-    #     teams[0] = request.form['team1']
-    #     teams[1] = request.form['team2']
-    #     # setPath()
-    #     return redirect(url_for('predicc'))
+    if request.method == "POST":
+        c, conn = connection()
+        email = request.form['email']
+        password = request.form['password']
+        print(email, password)
+        return render_template("login.html")
     return render_template("login.html")
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phoneNumber = request.form['phoneNumber']
+        password = request.form['pwd1']
+        print(name,email,phoneNumber,password)
+        return render_template("register.html")
     return render_template("register.html")
 
 @app.route('/guestLogin', methods=['GET', 'POST'])
