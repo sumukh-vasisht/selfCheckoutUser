53336ddba14b99e5306a80d1d5b6f591daa4fc7e
sumukh-vasisht <sumukhmys1999@gmail.com>
2020-05-23 18:35:42 +0530

    Backend Complete

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
+import json
+from datetime import timedelta
+import time
+import pyrebase
+import firebase_admin as firebase
+from firebase_admin import firestore, storage
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-import os
-import firebase_admin as firebase
-from firebase_admin import firestore
-import pyrebase
-import json
 
 #FOR FIRESTORE
 cred = firebase.credentials.Certificate("firebaseKey.json")
-firebase.initialize_app(cred)
-db = firestore.client()
-
-#FOR AUTH
 keyFile = open('pyrebaseKey.json','r')
 keyJson = keyFile.read()
 key = json.loads(keyJson)
-auth = pyrebase.initialize_app(key).auth()
+
+firebase.initialize_app(cred,{'storageBucket':'selfcheckout-8b125.appspot.com'})
+db = firestore.client() #For database
+bucket = storage.bucket()
+auth = pyrebase.initialize_app(key).auth() #For authentication
 
 #FIRESTORE COMMANDS
 # db.collection('collectionName').document('documentID').set(data(as a dictionary)) -- To create/edit document
@@ -34,6 +35,8 @@ auth = pyrebase.initialize_app(key).auth()
 # auth.sign_in_with_email_and_password(email, password) -- Sign in a user, returns a session key that expires in 60 mins I think.
 
 app = Flask(__name__)
+
+app.secret_key = "(SHITTU_CANNOT_CODE)-1"
 
 def sendEmail(email, html, text):
     senderAddress = "selfcheckoutsample@gmail.com"
@@ -52,14 +55,43 @@ def sendEmail(email, html, text):
     print('Email Sent')
     server.quit()
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
+def home():
+    if 'token' in session:
+        # print(session['token'])
+        # email = session['email']
+        # billDetails = db.collection('bills').document(email).get().to_dict()
+        # print(billDetails)
+        return render_template('home.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == "POST":
-        email = request.form['username']
-        password = request.form['password']
-        print(email, password)
-        return render_template("home.html")
-    return render_template("login.html")
+    if 'token' not in session:
+        message = ""
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            try:
+                userData = auth.sign_in_with_email_and_password(username,password)
+                userDetails = db.collection('users').document(username).get().to_dict()
+                print(userDetails)
+                session['name'] = userDetails['name']
+                session['email'] = userDetails['email']
+                session['phoneNumber'] = userDetails['phoneNumber']
+                session['age'] = userDetails['age']
+                session['address'] = userDetails['address']
+                session['token'] = userData['idToken']
+                print(dict(session))
+                return redirect('/')
+            except Exception as e:
+                error = ""
+                error = json.loads(e.args[1])['error']['message']
+                message = error.replace('_',' ')
+        return render_template('login.html',message=message)
+    else:
+        return redirect('/')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -70,7 +102,7 @@ def register():
         age = request.form['age']
         address = request.form['address']
         password = request.form['pwd1']
-        print(name,email,phoneNumber,password)
+        print(name,email,phoneNumber,age,address,password)
         text = """
         Dear %s,
         Thank you for signing-up on self-checkout!
@@ -96,16 +128,28 @@ def register():
         </html>
         """ %name
         sendEmail(email, html, text)
-        return render_template("home.html")
+        auth.create_user_with_email_and_password(email,password)
+        db.collection(u'users').document(email).set({
+            'name':name,
+            'email':email,
+            'phoneNumber':phoneNumber,
+            'age':age,
+            'address':address,
+            'billQuantity':0
+        })
+        userData = auth.sign_in_with_email_and_password(email,password) 
+        session['name'] = name
+        session['email'] = email
+        session['phoneNumber'] = phoneNumber
+        session['age'] = age
+        session['address'] = address
+        session['token'] = userData['idToken']
+        return redirect(url_for('home'))
     return render_template("register.html")
 
 @app.route('/guestLogin', methods=['GET', 'POST'])
 def guestLogin():
     return render_template("guestLogin.html")
-
-@app.route('/home', methods=['GET','POST'])
-def home():
-    return render_template("home.html")
 
 @app.route('/contact', methods=['GET','POST'])
 def contact():
@@ -159,11 +203,95 @@ def contact():
 
 @app.route('/viewCurrentBill', methods=['GET','POST'])
 def viewCurrentBill():
+    if 'token' in session:
+        email = session['email']
+        userDetails = db.collection('users').document(email).get().to_dict()
+        currentBillNumber = userDetails['billQuantity']
+        if currentBillNumber==0:
+            message = "There are no bills yet"
+            return render_template('viewCurrentBill.html', date=message,items=[], quantity=[], prices=[], totalPrice=0, leng=0)
+        documentID = email+str(currentBillNumber)
+        billDetails = db.collection('bills').document(documentID).get().to_dict()
+        date=billDetails['date']
+        totalPrice=billDetails['totalPrice']
+        number = len(billDetails)
+        numberOfItems = number-2        
+        items=['0' for i in range(numberOfItems//3)]
+        quantity=['0' for i in range(numberOfItems)]
+        prices=['0' for i in range(numberOfItems)]
+        for key,value in billDetails.items():
+            if 'item' in key:
+                for z in key:
+                    if z.isdigit():
+                        items[int(z)-1]=value
+            elif 'quantity' in key:
+                for z in key:
+                    if z.isdigit():
+                        quantity[int(z)-1]=value
+            elif 'price' in key:
+                for z in key:
+                    if z.isdigit():
+                        prices[int(z)-1]=value                        
+        return render_template('viewCurrentBill.html',date=date, items=items, quantity=quantity, prices=prices, totalPrice=totalPrice, leng=len(items))
+    return render_template("viewCurrentBill.html")
+
+@app.route('/viewCurrentBillFromAllBills/<string:id>', methods=['GET','POST'])
+def viewCurrentBillFromAllBills(id):
+    if 'token' in session:
+        email = session['email']
+        userDetails = db.collection('users').document(email).get().to_dict()
+        documentID = email+id
+        billDetails = db.collection('bills').document(documentID).get().to_dict()
+        date=billDetails['date']
+        totalPrice=billDetails['totalPrice']
+        number = len(billDetails)
+        numberOfItems = number-2        
+        items=['0' for i in range(numberOfItems//3)]
+        quantity=['0' for i in range(numberOfItems)]
+        prices=['0' for i in range(numberOfItems)]
+        for key,value in billDetails.items():
+            if 'item' in key:
+                for z in key:
+                    if z.isdigit():
+                        items[int(z)-1]=value
+            elif 'quantity' in key:
+                for z in key:
+                    if z.isdigit():
+                        quantity[int(z)-1]=value
+            elif 'price' in key:
+                for z in key:
+                    if z.isdigit():
+                        prices[int(z)-1]=value                        
+        return render_template('viewCurrentBill.html',date=date, items=items, quantity=quantity, prices=prices, totalPrice=totalPrice, leng=len(items))
     return render_template("viewCurrentBill.html")
 
 @app.route('/viewAllBills', methods=['GET','POST'])
 def viewAllBills():
+    if 'token' in session:
+        email = session['email']
+        userDetails = db.collection('users').document(email).get().to_dict()
+        currentBillNumber=userDetails['billQuantity']
+        documentIDs=[]
+        for i in range(currentBillNumber):
+            documentIDs.append(email+str(i+1))
+        dates=[]
+        for i in range(len(documentIDs)):
+            billDetails = db.collection('bills').document(documentIDs[i]).get().to_dict()
+            date=billDetails['date']
+            dates.append(date)
+        return render_template("viewAllBills.html", dates=dates, leng=len(dates))
     return render_template("viewAllBills.html")
+
+@app.route('/logout')
+def logout():
+    if 'token' in session:
+        session.pop('token')
+        session.pop('name')
+        session.pop('email')
+        session.pop('phoneNumber')
+        session.pop('age')
+        session.pop('address')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
